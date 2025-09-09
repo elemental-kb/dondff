56f86893722e432c11d680f7105b601edb33da4c
Foxsly <timothylbelcher@gmail.com>
2025-09-09 09:58:34 -0400

    hide add season button if season already exists

--- src/components/seasons.js
+++ src/components/seasons.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
-import { db } from "../firebase-config";
+import {auth, db} from "../firebase-config";
 import { collection, doc, setDoc } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { Link } from "react-router-dom";
 
 const Seasons = ({ leagueId }) => {
+  const user = auth.currentUser;
   const [year, setYear] = useState("");
 
   const leagueCollection = collection(db, "leagues", leagueId, "seasons");
   const [docs, loading] = useCollectionData(leagueCollection);
+
+  const membersCollection = collection(db, "leagues", leagueId, "members");
+  const [members] = useCollectionData(membersCollection, { idField: "id" });
+  const currentMember = members?.find((m) => m.uid === user?.uid);
+  const isAdmin = currentMember?.role === "admin";
 
   const addSeason = async (year) => {
     setYear(year);
@@ -37,12 +43,14 @@ const Seasons = ({ leagueId }) => {
         ))}
       </div>
 
-      <button
-        className="px-4 py-2 font-bold text-[#102131] bg-[#00ceb8] rounded hover:bg-[#00ceb8]/80"
-        onClick={() => addSeason("2025")}
-      >
-        Add 2025 Season
-      </button>
+      {isAdmin && !docs?.find((season) => season.season === "2025") && (
+        <button
+          className="px-4 py-2 font-bold text-[#102131] bg-[#00ceb8] rounded hover:bg-[#00ceb8]/80"
+          onClick={() => addSeason("2025")}
+        >
+          Add 2025 Season
+        </button>
+      )}
     </div>
   );
 };
